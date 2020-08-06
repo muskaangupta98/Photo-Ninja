e170e0aaacaa906c8b6d9e35bf87fdf1176b713b
Muskaan <muskaangupta98@gmail.com>
2020-08-06 15:59:51 +0530

    final commit

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".tooltipped").tooltip();
+  $(".materialboxed").materialbox();
+  $(".parallax").parallax();
+  $(".tabs").tabs();
+  $(".datepicker").datepicker();
+  $(".scrollspy").scrollSpy();
+});
